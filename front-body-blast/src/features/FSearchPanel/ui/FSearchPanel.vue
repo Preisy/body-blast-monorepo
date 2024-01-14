eec41ac7a7eba147d2e7d0edf6a9549ed65f16f4
max2556 <46759632+max2556@users.noreply.github.com>
2024-01-14 16:44:39 +0300

    🩹 FSearch onchange emit

--- front-body-blast/src/features/FSearchPanel/ui/FSearchPanel.vue
+++ front-body-blast/src/features/FSearchPanel/ui/FSearchPanel.vue
@@ -17,7 +17,13 @@ const onsubmit = () => emit('update:query', value.value);
 
 <template>
   <SComponentWrapper w-full flex gap-x-0.5rem>
-    <SInput v-model:model-value="value" name="query" w-full :label="$t('admin.profile.search.label')" />
+    <SInput
+      v-model:model-value="value"
+      @update:model-value="onsubmit"
+      name="query"
+      w-full
+      :label="$t('admin.profile.search.label')"
+    />
     <SBtn h-min icon="sym_r_search" type="submit" @click="onsubmit" />
   </SComponentWrapper>
 </template>
