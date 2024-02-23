bf4fe834f26dd0d5e3c648a583bd9b83e0580918
max2556 <46759632+max2556@users.noreply.github.com>
2024-02-23 16:12:09 +0300

    🩹 useAuthLink reactivity fix

--- front-body-blast/src/shared/lib/hooks/useAuthLink.ts
+++ front-body-blast/src/shared/lib/hooks/useAuthLink.ts
@@ -1,17 +1,21 @@
+import { MaybeRefOrGetter } from 'vue';
 import { URLHandler, useFileStore } from 'shared/api/file';
 import { useLoadingAction } from 'shared/lib/loading';
 import { useSingleState } from 'shared/lib/utils';
 
-export const useAuthLink = (link: string) => {
+export const useAuthLink = (link: MaybeRefOrGetter<string> | ComputedRef<string>) => {
   const fileStore = useFileStore();
   const state = ref(useSingleState<URLHandler>());
-  const videoFileName = link.split('/').pop() || '';
 
-  useLoadingAction(state.value, () => fileStore.getFileByName({ filename: videoFileName }, state.value));
+  watchEffect(() => {
+    const videoFileName = computed(() => toValue(link).split('/').pop() || '');
+    console.log(videoFileName.value);
+    useLoadingAction(state.value, () => fileStore.getFileByName({ filename: videoFileName.value }, state.value));
+  });
 
   onUnmounted(() => {
     state.value.data?.destructor();
   });
 
-  return state;
+  return { state };
 };
