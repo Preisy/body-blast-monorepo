9c3c119d60ffe073b554dcc3b1dcfa746e7c7226
SteklotaraBlizz <zuxxlyes2015@gmail.com>
2024-03-09 21:00:13 +0300

    :fire: Removed Multer import from logs module

--- back-erax-fit/src/modules/admin/logging/admin-logs.module.ts
+++ back-erax-fit/src/modules/admin/logging/admin-logs.module.ts
@@ -1,15 +1,9 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
 import { AdminLogsController } from './admin-logs.controller';
 import { AuthModule } from '../../../modules/authentication/auth.module';
 
 @Module({
-  imports: [
-    forwardRef(() => AuthModule),
-    MulterModule.register({
-      dest: './logs',
-    }),
-  ],
+  imports: [forwardRef(() => AuthModule)],
   controllers: [AdminLogsController],
 })
 export class AdminLogsModule {}
