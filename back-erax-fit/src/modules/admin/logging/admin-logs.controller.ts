eb8c3b52041ea9f7c890b967bb17b343d8852371
SteklotaraBlizz <zuxxlyes2015@gmail.com>
2024-03-09 20:17:43 +0300

    :zap: Minor fixes with logs

--- back-erax-fit/src/modules/admin/logging/admin-logs.controller.ts
+++ back-erax-fit/src/modules/admin/logging/admin-logs.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Get, Header, Param, ParseIntPipe, Res, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Header, ParseIntPipe, Query, Res, StreamableFile } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppResponses } from '../../../decorators/app-responses.decorator';
 import type { Response } from 'express';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 import * as fs from 'fs';
+import { BaseAuthGuard } from '../../../modules/authentication/guards/baseAuth.guard';
+import { RoleGuard } from '../../../modules/authentication/guards/role.guard';
+import { UserRole } from '../../../constants/constants';
 
 @Controller('admin/logs')
 @ApiTags('Admin logs')
+@BaseAuthGuard(RoleGuard(UserRole.Admin))
 export class AdminLogsController {
-  @Get(':limit')
-  @AppResponses({ status: 200, type: 'file' })
-  async getSomeLogs(@Res() res: Response, @Param('limit', ParseIntPipe) limit: number, @Param('page') page: number) {
+  @Get()
+  @AppResponses({ status: 200, type: String })
+  async getSomeLogs(
+    @Res() res: Response,
+    @Query('limit', ParseIntPipe) limit: number,
+    @Query('page', ParseIntPipe) page: number,
+  ) {
     const data = await new Promise<string>((res, rej) =>
       fs.readFile('./logs/log.txt', 'utf-8', (err, data) => {
         if (err) rej(err);
@@ -20,12 +28,12 @@ export class AdminLogsController {
     );
     const skip = (page - 1) * limit! || 0;
     const lines = data.split('\n');
-    const writableData = lines.slice(lines.length - (limit - skip), lines.length - skip).join('\n');
+    const writableData = lines.slice(lines.length - limit * page, lines.length - skip).join('\n');
     return res.send(writableData);
   }
 
-  @Get()
-  @Header('Content-Type', 'application/txt')
+  @Get('file')
+  @AppResponses({ status: 200 })
   @Header('Content-Disposition', 'attachment; filename="log.txt"')
   getLogFile(): StreamableFile {
     const file = createReadStream(join(process.cwd(), './logs/log.txt'));
