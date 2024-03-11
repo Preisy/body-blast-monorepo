e8e00de214e8b97b27133211b09f94248d00db8f
max2556 <46759632+max2556@users.noreply.github.com>
2024-03-11 14:40:05 +0300

    🩹 PProfileEdit

--- front-body-blast/src/shared/api/me/types.ts
+++ front-body-blast/src/shared/api/me/types.ts
@@ -4,5 +4,11 @@ import { User } from '../user';
 export interface Me extends User {}
 
 export namespace Me {
-  export interface Response extends AppBaseEntity.Response<Me> {}
+  export namespace Get {
+    export interface Response extends AppBaseEntity.Response<Me> {}
+  }
+  export namespace Patch {
+    export interface Dto extends Partial<Omit<User, 'email' | 'password' | 'firstName' | 'lastName'>> {}
+    export interface Response extends AppBaseEntity.Response<Me> {}
+  }
 }
