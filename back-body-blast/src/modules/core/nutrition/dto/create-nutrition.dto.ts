cd677d80380cc6e4941b19814bc52c5c7e0b86d8
SteklotaraBlizz <zuxxlyes2015@gmail.com>
2024-03-13 19:26:30 +0300

    :bug: Removed @Min validators from string fields

--- back-body-blast/src/modules/core/nutrition/dto/create-nutrition.dto.ts
+++ back-body-blast/src/modules/core/nutrition/dto/create-nutrition.dto.ts
@@ -1,13 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { ArrayNotEmpty, IsDefined, IsString, Length, Min, ValidateNested } from 'class-validator';
+import { ArrayNotEmpty, IsDefined, IsString, Length, ValidateNested } from 'class-validator';
 import { CreateMealItemRequest } from './create-meal-item.dto';
 
 export class CreateNutritionRequest {
   @IsDefined()
   @ApiProperty()
   @IsString()
-  @Min(1)
   public userId: string;
 
   @IsDefined()
