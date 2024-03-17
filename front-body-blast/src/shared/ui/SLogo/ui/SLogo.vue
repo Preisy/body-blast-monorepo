ae1799ce90851fbe9680d0d48ecd39acf41b8e48
max2556 <46759632+max2556@users.noreply.github.com>
2024-03-17 15:13:07 +0300

    🩹 no result select-none, logo svg

--- front-body-blast/src/shared/ui/SLogo/ui/SLogo.vue
+++ front-body-blast/src/shared/ui/SLogo/ui/SLogo.vue
@@ -2,6 +2,6 @@
 
 <template>
   <div>
-    <q-img src="/Logo.png" h-5rem w-5rem />
+    <q-img src="/Logo.svg" h-5rem w-5rem />
   </div>
 </template>
--- front-body-blast/src/shared/ui/SNoResultsScreen/ui/SNoResultsScreen.vue
+++ front-body-blast/src/shared/ui/SNoResultsScreen/ui/SNoResultsScreen.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div flex items-center justify-center>
+  <div flex select-none items-center justify-center>
     <h1 text-1.5rem>{{ $t('shared.noresults.message') }}</h1>
   </div>
 </template>
