9d8a0929346764ec6ffdff277861835bda6618a3
max2556 <46759632+max2556@users.noreply.github.com>
2024-03-20 19:45:01 +0300

    ➕ moment-timezone, dates fix

--- front-body-blast/src/shared/lib/utils/isToday.ts
+++ front-body-blast/src/shared/lib/utils/isToday.ts
@@ -1,4 +1,4 @@
-import moment, { Moment } from 'moment';
+import moment, { Moment } from 'moment-timezone';
 
 /**
  * Checks if certain date is today
@@ -14,17 +14,18 @@ export function isToday(date: Moment): boolean;
 
 export function isToday(date: string | Moment) {
   const isoString = typeof date === 'string' ? date.split('/').join('-') : undefined;
-  const dateMoment = typeof date === 'string' ? moment(isoString).utc(true).utcOffset(-3, true) : date;
+  const dateMoment = typeof date === 'string' ? getUTC3Date(isoString) : date;
   const today = getUTC3Date();
 
-  return today.isSame(dateMoment, 'days') && today.isSame(dateMoment, 'months') && today.isSame(dateMoment, 'years');
+  return isEqualDates(today, dateMoment);
 }
 
 export function isEqualDates(date1: string | Moment, date2: string | Moment) {
   const isoString1 = typeof date1 === 'string' ? date1.split('/').join('-') : undefined;
-  const dateMoment1 = typeof date1 === 'string' ? moment(isoString1) : date1;
+  const dateMoment1 = typeof date1 === 'string' ? getUTC3Date(isoString1) : date1;
   const isoString2 = typeof date2 === 'string' ? date2.split('/').join('-') : undefined;
-  const dateMoment2 = typeof date2 === 'string' ? moment(isoString2) : date2;
+  const dateMoment2 = typeof date2 === 'string' ? getUTC3Date(isoString2) : date2;
+
   return (
     dateMoment1.isSame(dateMoment2, 'days') &&
     dateMoment1.isSame(dateMoment2, 'months') &&
@@ -32,6 +33,6 @@ export function isEqualDates(date1: string | Moment, date2: string | Moment) {
   );
 }
 
-export function getUTC3Date() {
-  return moment().utc().utcOffset(-3, true);
+export function getUTC3Date(iso?: string) {
+  return iso ? moment(iso).tz('Europe/Moscow', true) : moment().tz('Europe/Moscow');
 }
