2ce5d308edafc36e06df4e5b630488bbe24e01b9
max2556 <46759632+max2556@users.noreply.github.com>
2024-05-09 16:06:31 +0300

    🚚 features/notification service

--- front-body-blast/src/features/notification/api/index.ts
+++ front-body-blast/src/features/notification/api/index.ts
@@ -1,7 +1,1 @@
-import { api } from 'shared/config';
-import { useServiceAction } from 'shared/lib';
-import { Notification } from '..';
-
-export const notificationService = {
-  getNotifications: useServiceAction(() => api.get<Notification.Get.Response>(`/me/notifications`)),
-};
+export * from './service';
