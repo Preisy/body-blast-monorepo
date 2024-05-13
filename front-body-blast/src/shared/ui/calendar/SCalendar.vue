cb0050a4ca1a40706b84ba9b77f1e97200ff7cbe
max2556 <46759632+max2556@users.noreply.github.com>
2024-05-13 18:24:04 +0300

    🩹 q-popup-proxy -> qdialog

--- front-body-blast/src/shared/ui/calendar/SCalendar.vue
+++ front-body-blast/src/shared/ui/calendar/SCalendar.vue
@@ -36,21 +36,12 @@ const getDate = (td: string) => {
 
 <template>
   <div flex justify-center>
-    <div w-min flex items-center justify-center gap-2 @click="showDateModal = true">
+    <div @click="showDateModal = true" w-min flex cursor-pointer items-center justify-center gap-2>
       <p text-center font-800>{{ getDate(dateValue) }}</p>
       <q-icon name="calendar_month" text-black />
     </div>
-    <q-popup-proxy cover v-if="showDateModal">
-      <q-date
-        v-bind="$props"
-        absolute
-        left="1/2"
-        top="1/2"
-        translate-x="-1/2"
-        translate-y="-1/2"
-        z-100
-        v-model="dateValue"
-      />
-    </q-popup-proxy>
+    <q-dialog v-model="showDateModal">
+      <q-date v-bind="$props" v-model="dateValue" left="1/2" top="1/2" translate-x="-1/2" translate-y="-1/2" absolute />
+    </q-dialog>
   </div>
 </template>
