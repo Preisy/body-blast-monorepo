a5c4b226d53aebe64e3e9bd9836043473b7dac6f
Jacobzzzzzzz <yakov.komar@mail.ru>
2024-06-08 17:25:57 +0300

    :sparkles: Added AbilityFactory

---
+++ back-body-blast/src/modules/ability/ability.factory.ts
@@ -0,0 +1,36 @@
+import { AbilityBuilder, createMongoAbility, InferSubjects, MongoAbility } from '@casl/ability';
+import { UserEntity } from '../core/user/entities/user.entity';
+import { WorkoutEntity } from '../core/workout/entity/workout.entity';
+import { Injectable } from '@nestjs/common';
+
+export enum Action {
+  Manage = 'manage',
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+type Subjects = InferSubjects<typeof WorkoutEntity> | 'all';
+export type Ability = MongoAbility<[Action, Subjects]>;
+
+@Injectable()
+export class AbilityFactory {
+  defineAbility(user: UserEntity) {
+    const { can, cannot, build } = new AbilityBuilder<Ability>(createMongoAbility);
+    if (user.role == 'admin') {
+      can(Action.Manage, WorkoutEntity);
+      cannot(Action.Update, WorkoutEntity, ['comment']);
+    } else if (user.role == 'client') {
+      can(Action.Read, WorkoutEntity);
+      cannot(Action.Read, WorkoutEntity, { userId: { $ne: user.id } }).because(
+        'Sorry, you can only see your own workouts',
+      );
+
+      can(Action.Update, WorkoutEntity, ['comment']);
+      cannot(Action.Update, WorkoutEntity, { userId: { $ne: user.id } }).because(
+        'Sorry, you can only comment your own workouts',
+      );
+    }
+  }
+}
