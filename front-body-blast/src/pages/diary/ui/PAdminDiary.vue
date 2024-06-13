90c2f27dc6ece2d4cd15e1f092e8a5f80ba27594
max2556 <46759632+max2556@users.noreply.github.com>
2024-06-13 17:23:28 +0300

    🩹 PAdminDiary pagination

--- front-body-blast/src/pages/diary/ui/PAdminDiary.vue
+++ front-body-blast/src/pages/diary/ui/PAdminDiary.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import moment, { Moment } from 'moment';
+import { Dictionary, groupBy } from 'lodash';
+import moment from 'moment';
 import { ESelfControlList, EStepsList, Diary, useAdminDiaryStore } from 'entities/diary';
 import { useAdminUserStore, AppBaseEntity } from 'shared/api';
 import { useLoadingAction, toWeekRange } from 'shared/lib';
-import { SCalendar, SStructure, SProxyScroll } from 'shared/ui';
+import { SCalendar, SStructure, SProxyScroll, SDatePagination } from 'shared/ui';
 
 export interface PAdminDiaryProps {
   id: AppBaseEntity['id'];
@@ -12,44 +13,45 @@ const props = defineProps<PAdminDiaryProps>();
 const { getUserSteps, userSteps } = useAdminUserStore();
 const { getUserDiaries, userDiaries } = useAdminDiaryStore();
 
-// 2024-02-01T23:59:59.000Z
-const dateRaw = ref<Moment>(moment());
-const calendarDate = computed(() => dateRaw.value.format('YYYY/MM/DD'));
-// 2024-02-01T00:00:00.000Z
-const date = computed(() => dateRaw.value.clone().utcOffset(0).date(1).hour(0).minute(0).seconds(0).milliseconds(0));
-// 2024-03-01T00:00:00.000 <- Difference with 'date' only in MM field
-const dateMonthPlusOne = computed(() => date.value.clone().add(1, 'M'));
+const dateRaw = ref(moment());
+const isoDate = ref(moment().format('YYYY-MM-DD'));
+const calendarDate = computed(() => moment(isoDate.value).format('YYYY/MM/DD'));
 
-watch(
-  dateMonthPlusOne,
-  () => {
-    useLoadingAction(userSteps, async () => {
-      //get all diaries in month range
-      await getUserDiaries({
-        id: props.id,
-        pagination: { expanded: true, from: date.value.toISOString(), to: dateMonthPlusOne.value.toISOString() },
-      });
-      //get all steps in month range
-      return getUserSteps({
-        id: props.id,
-        pagination: { expanded: true, from: date.value.toISOString(), to: dateMonthPlusOne.value.toISOString() },
-      });
-    });
-  },
-  { immediate: true },
-);
+const fetch = (from: string, to: string) => {
+  useLoadingAction(userDiaries, async () => {
+    await Promise.allSettled([
+      getUserDiaries({ id: props.id, pagination: { expanded: true, from, to } }),
+      getUserSteps({ id: props.id, pagination: { expanded: true, from, to } }),
+    ]);
+  });
+};
 
 const diariesData = computed(() => userDiaries.data?.data);
 const stepsData = computed(() => userSteps.data);
 
 const diariesSlides = computed(() => {
-  const diariesSlides: Record<string, Array<Diary>> = {};
-  diariesData.value?.forEach((slide) => {
-    const week = toWeekRange(slide.date);
-    if (week in diariesSlides) diariesSlides[week].push(slide);
-    else diariesSlides[week] = [slide];
+  const groupedByMonth = groupBy(diariesData.value, ({ date }) => moment(date).format('YYYY-MM')) as Dictionary<
+    Diary[] | Dictionary<Diary[]>
+  >;
+  for (const month in groupedByMonth) {
+    const diariesInMonth = groupedByMonth[month] as Diary[];
+    groupedByMonth[month] = groupBy(diariesInMonth, ({ date }) => toWeekRange(date));
+  }
+  return groupedByMonth as Dictionary<Dictionary<Diary[]>>;
+});
+const stepsSlides = computed(() => {
+  //TODO: api issue in steps response
+  // startDate: 19.02
+  // endDate: 25.05
+  // month applied only to endDate
+  const groupedByMonth = groupBy(stepsData.value?.weeks, ({ endDate }) => {
+    const [dd, mm] = endDate.split('.');
+    const isoEndDate = `${moment().year()}-${mm}-${dd}`;
+    if (dd == '01') return moment(isoEndDate).subtract(1, 'month').format('MM');
+    else return moment(isoEndDate).format('MM');
   });
-  return diariesSlides;
+
+  return groupedByMonth;
 });
 </script>
 
@@ -64,9 +66,18 @@ const diariesSlides = computed(() => {
       py-1rem
     />
 
-    <SProxyScroll h-full px-1.5rem pb-1.5rem>
-      <EStepsList :weeks="stepsData?.weeks ?? []" mb-2rem />
-      <ESelfControlList v-for="(slide, week) in diariesSlides" :key="week" :slides="slide" :week="week" />
-    </SProxyScroll>
+    <SDatePagination v-model="isoDate" :half-range="1" :offset="0" @need-fetch="fetch" type="months">
+      <template #item="{ date: month }">
+        <SProxyScroll h-full>
+          <EStepsList :weeks="stepsSlides[moment(month).format('MM')] ?? []" mb-2rem />
+          <ESelfControlList
+            v-for="(slides, week) in diariesSlides[moment(month).format('YYYY-MM')]"
+            :key="week"
+            :slides="slides"
+            :week="'' + week"
+          />
+        </SProxyScroll>
+      </template>
+    </SDatePagination>
   </SStructure>
 </template>
