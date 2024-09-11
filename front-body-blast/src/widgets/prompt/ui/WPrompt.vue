621c10a1f7f9c8258a9b01597b70aa8bc7217ea4
max2556 <46759632+max2556@users.noreply.github.com>
2024-09-11 17:31:27 +0300

    SVideoWithPreview component

--- front-body-blast/src/widgets/prompt/ui/WPrompt.vue
+++ front-body-blast/src/widgets/prompt/ui/WPrompt.vue
@@ -5,23 +5,17 @@ import {
   symRoundedPause,
   symRoundedPlayArrow,
 } from '@quasar/extras/material-symbols-rounded';
-import { useAuthLink } from 'entities/file';
 import { Prompt, useAdminPromptStore } from 'entities/prompt';
 import { AppBaseEntity } from 'shared/api';
-import { SBtn, SLoading, SVideo } from 'shared/ui';
+import { SBtn, SVideoWithPreview } from 'shared/ui';
 import PromptEditDialog from './PromptEditDialog.vue';
 
 export interface WPromptsProps {
   prompt: Prompt;
 }
-const props = defineProps<WPromptsProps>();
+defineProps<WPromptsProps>();
 
 const { deletePrompt } = useAdminPromptStore();
-
-const { state: video } = useAuthLink(() => props.prompt.videoLink);
-const { state: photo } = useAuthLink(() => props.prompt.photoLink);
-
-const videoControl = ref<InstanceType<typeof SVideo>>();
 
 // Edit dialog data
 const isEditDialogOpen = ref<boolean>(false);
@@ -35,41 +29,14 @@ const onDeleteClick = async (id: AppBaseEntity['id']) => {
   deletePrompt({ id });
 };
 
-const isModalShown = ref(false);
+const videoControl = ref<InstanceType<typeof SVideoWithPreview>>();
 </script>
 
 <template>
   <div>
     <h2 mb-0.75rem>{{ prompt.type }}</h2>
-    <div relative>
-      <div v-if="video.data && photo.data" overflow-hidden rounded-1rem>
-        <SVideo
-          ref="videoControl"
-          :link-url="video.data.link"
-          disable-btn
-          absolute
-          h-full
-          w-full
-          overflow-hidden
-          top="50%"
-          left="50%"
-          translate="-50%"
-        />
-        <q-img
-          @click="isModalShown = true"
-          :src="photo.data.link"
-          :class="{ 'opacity-0 z--1': videoControl?.isPlaying }"
-          h-auto
-          max-h-20rem
-          w-full
-          overflow-hidden
-          rounded-1rem
-        />
-      </div>
-      <template v-else>
-        <SLoading />
-      </template>
-    </div>
+
+    <SVideoWithPreview ref="videoControl" :video-link="prompt.videoLink" :photo-link="prompt.photoLink" />
 
     <div mx-5px mt-0.5rem flex flex-row gap-x-0.5rem>
       <SBtn
@@ -83,8 +50,5 @@ const isModalShown = ref(false);
 
     <!-- onEditPopup -->
     <PromptEditDialog v-if="editPromptData" v-model="isEditDialogOpen" :prompt="editPromptData" />
-    <q-dialog v-model="isModalShown">
-      <q-img :src="photo.data?.link" @click="isModalShown = false" overflow="hidden!" rounded="1.5rem!" />
-    </q-dialog>
   </div>
 </template>
